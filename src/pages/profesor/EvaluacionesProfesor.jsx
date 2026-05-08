4ed5460b8e333a8cd36953845fa20a84c1aa5c11
Cristian Candela <cristiancandela561@gmail.com>
2026-05-08 13:25:06 -0500

    Se añadio vistas y carpeta profesor

---
+++ src/pages/profesor/EvaluacionesProfesor.jsx
@@ -0,0 +1,14 @@
+import Sidebar from "../../components/Sidebar";
+
+export default function EvaluacionesProfesor() {
+  return (
+    <div className="d-flex">
+        <Sidebar role="profesor" />
+
+        <div className="flex-grow-1 p-5" style={{ marginLeft: "70px" }}>
+            <h1 className="fw-bold text-indigo">Panel del Profesor</h1>
+            <p>Aquí podrás ver tus evaluaciones y recomendaciones.</p>
+        </div>
+    </div>
+  );
+}

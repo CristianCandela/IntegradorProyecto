449d358b821135be641c7cf5aec7aeb4c574a01a
Cristian Candela <cristiancandela561@gmail.com>
2026-05-07 21:31:33 -0500

    Se elimino el uso del Navbar y Footer en paginas donde no son necesarias para correcto funcionamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Inicio from "./pages/public/Inicio";
@@ -6,21 +6,33 @@ import Nosotros from "./pages/public/Nosotros";
 import Login from "./pages/public/Login";
 import Registro from "./pages/public/Registro";
 
+
+function LayoutWrapper({ children }) {
+  const location = useLocation();
+  const hideLayout = location.pathname === "/login" || location.pathname === "/registro";
+
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      {!hideLayout && <Navbar />}
+      <div className="flex-grow-1">
+        {children}
+      </div>
+      {!hideLayout && <Footer />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <Navbar />
-        <div className="flex-grow-1">
-          <Routes>
-            <Route path="/" element={<Inicio />} />
-            <Route path="/nosotros" element={<Nosotros />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/registro" element={<Registro />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
+      <LayoutWrapper>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Registro />} />
+        </Routes>
+      </LayoutWrapper>
     </Router>
   );
 }

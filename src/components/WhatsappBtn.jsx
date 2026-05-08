5fcd78d61e2fdf3663507792c6722a6e88eb5560
Cristian Candela <cristiancandela561@gmail.com>
2026-05-07 22:24:04 -0500

    Se modifico el boton componente whatsapp

--- src/components/WhatsappBtn.jsx
+++ src/components/WhatsappBtn.jsx
@@ -1,13 +1,12 @@
+import React from 'react';
+
 export default function WhatsappBtn() {
-  const phone = "51900000000"; // Cambia por tu número
-  const msg = "Hola ProfeMatch, necesito ayuda con mi acceso.";
-  
   return (
     <a 
-      href={`https://wa.me/${phone}?text=${encodeURIComponent(msg)}`}
-      target="_blank"
+      href="https://wa.me/51900000000" 
+      className="whatsapp-float" 
+      target="_blank" 
       rel="noopener noreferrer"
-      className="whatsapp-float"
     >
       <i className="bi bi-whatsapp"></i>
     </a>

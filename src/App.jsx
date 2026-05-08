a7548c7d1558b7b1e2d92c43f5e50a8d40d993d9
Cristian Candela <cristiancandela561@gmail.com>
2026-05-08 12:04:58 -0500

    Se añaden Rutas de Inicio para Admin, estudiante y profesor

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,47 @@ import Nosotros from "./pages/public/Nosotros";
 import Login from "./pages/public/Login";
 import Registro from "./pages/public/Registro";
 
+import InicioAdmin from "./pages/admin/InicioAdmin";
+import InicioEstudiante from "./pages/estudiante/InicioEstudiante";
+import InicioProfesor from "./pages/profesor/InicioProfesor";
 
 function LayoutWrapper({ children }) {
+
   const location = useLocation();
-  const hideLayout = location.pathname === "/login" || location.pathname === "/registro";
+
+  const hideLayout = [
+  "/login",
+  "/registro",
+  "/inicio-admin",
+  "/inicio-estudiante",
+  "/inicio-profesor"
+  ].includes(location.pathname);
 
   return (
-    <div className="d-flex flex-column min-vh-100">
-      {!hideLayout && <Navbar />}
-      <div className="flex-grow-1">
-        {children}
-      </div>
-      {!hideLayout && <Footer />}
-    </div>
+  <div className="d-flex flex-column min-vh-100">
+    {!hideLayout && <Navbar />}
+      <div className="flex-grow-1"> {children} </div>
+    {!hideLayout && <Footer />}
+  </div>
   );
 }
 
 function App() {
+
   return (
     <Router>
       <LayoutWrapper>
         <Routes>
+          {/* Rutas Públicas */}
           <Route path="/" element={<Inicio />} />
           <Route path="/nosotros" element={<Nosotros />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Registro />} />
+
+          {/* Rutas Privadas (Nuevas) */}
+          <Route path="/inicio-admin" element={<InicioAdmin />} />
+          <Route path="/inicio-estudiante" element={<InicioEstudiante />} />
+          <Route path="/inicio-profesor" element={<InicioProfesor />} />
         </Routes>
       </LayoutWrapper>
     </Router>
@@ -38,3 +54,4 @@ function App() {
 }
 
 export default App;
+

141df7718ed2cf93213b2cc22edbcab6f515440e
Cristian Candela <cristiancandela561@gmail.com>
2026-05-08 13:24:44 -0500

    Se añadio vistas y carpeta admin

---
+++ src/pages/admin/InicioAdmin.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "../../components/Sidebar";
+
+export default function InicioAdmin() {
+  return (
+    <div className="d-flex">
+        <Sidebar role="admin" />
+
+        <div className="flex-grow-1 p-5" style={{ marginLeft: "70px" }}>
+            <h1 className="fw-bold">Panel de Administrador</h1>
+            <p>Bienvenido, aquí gestionarás la plataforma ProfeMatch.</p>
+        </div>
+    </div>
+  );
+}
+
